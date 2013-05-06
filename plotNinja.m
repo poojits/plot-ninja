7c5ce9a91725affaee4e98d8ad91a0d9c78b8c89
Poojit Sharma <poojit@gmail.com>
2013-05-06 21:33:28 +0530

    supports variable number of plots. Max 5

--- plotNinja.m
+++ plotNinja.m
@@ -1,22 +1,17 @@
 function plotNinja(plots,names,titleText,xText,yText)
+	colors = [0,0,0 ; 1,0,0 ; 0,0,1 ; 0,0.498,0 ; 1,0,1];
+	markers = char('.','.','none','x','s');
+	linestyle = char('none','--','-','-','-');
+	numberOfPlots = size(plots,2);
+	plotHandle = zeros(numberOfPlots,1);
 	figure;
-	p1 = plot(plots(:,1)); %Baseline
-	hold on;
-	set(p1,'Color',[0,0,0],'LineStyle','none','Marker','.','MarkerSize',10);
-	hold on;
-	p2 = plot(plots(:,2)); %QPFS
-	hold on;
-	set(p2,'Color',[1,0,0],'LineStyle','--','LineWidth',2,'Marker','.','MarkerSize',10);
-	hold on;
-	p3 = plot(plots(:,3)); %TLKM-QPFS
-	hold on;
-	set(p3,'Color',[0,0,1],'LineStyle','-','LineWidth',2);
-	hold on;
-	p4 = plot(plots(:,4)); %IKM-QPFS
-	hold on;
-	set(p4,'Color',[0,0.498,0],'LineStyle','-','LineWidth',2,'Marker','x','MarkerSize',10);
-	hold on;
-	l = legend([p1;p2;p3;p4],names);
+	for i=1:numberOfPlots
+		plotHandle(i) = plot(plots(:,i));
+		hold on;
+		set(plotHandle(i),'Color',colors(i,:),'LineWidth',2,'LineStyle',linestyle(i,:),'Marker',markers(i,:),'MarkerSize',10);
+		hold on;
+	end
+	l = legend(plotHandle,names);
 	hold on;
 	set(l,'FontSize',30);
 	title(titleText);
